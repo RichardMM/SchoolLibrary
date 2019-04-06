30e50fc072187835af356315556e04ff0c3a68e7
RichardMM <rmachaz@gmail.com>
2019-04-06 13:18:17 +0300

    Migrations to mysql

--- SchoolLibrary/Models/Context.cs
+++ SchoolLibrary/Models/Context.cs
@@ -1,13 +1,23 @@
 ﻿using System.Data.Entity;
-using System.Data.SQLite;
+
+using MySql.Data.EntityFramework;
 
 namespace SchoolLibrary.Models
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     class LibAppContext:DbContext
 
     {
+        public LibAppContext():base("name=LibAppContext")
+        {
+
+        }
+
         public DbSet<Book> Books { get; set; }
         public DbSet<User> Users { get; set; }
+
+
+
 
     }
 }
